76b85326a1fceaeb6dd459794cacc22d9fd60c5f
usavk <113962032+usavk@users.noreply.github.com>
2023-05-05 08:50:46 +0530

    Add files via upload

---
+++ matrix mul recursion.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+void mul(int r1,int r2,int c1,int c2,int a[20][20],int b[20][20],int c[20][20])
+{
+	int i,j,k;
+	if(c1!=r2)
+	{
+		printf("its not possible");
+	}
+	else
+	{
+		for(i=0;i<r1;i++)
+		{
+			for(j=0;j<c2;j++)
+			{
+				c[i][j]=0;
+				for(k=0;k<c1;k++)
+				{
+					c[i][j]+=a[i][k]*b[k][j];
+				}
+			}
+		}
+	}
+}
+int main()
+{
+	int a[20][20],b[20][20],c[20][20],i,j,k,r1,r2,c1,c2;
+	printf("enter the no of rows and cols in matrix 1:");
+	scanf("%d%d",&r1,&c1);
+	printf("enter the values in the first matrix:");
+	for(i=0;i<r1;i++)
+	{
+		for(j=0;j<c1;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("enter the no of rows and cols in matrix 2:");
+	scanf("%d%d",&r2,&c2);
+	printf("enter the values in the second matrix:");
+	for(i=0;i<r2;i++)
+	{
+		for(j=0;j<c2;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+		printf("\n");
+	}
+	mul(r1,r2,c1,c2,a,b,c);
+		for(i=0;i<r1;i++)
+		{
+			for(j=0;j<c2;j++)
+			{
+				printf("%d\t",c[i][j]);
+			}
+			printf("\n");
+		}
+	}
